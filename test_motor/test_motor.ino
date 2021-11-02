db14595e0a6cf207f904fed43f0f2ff51a3dafca
Annabelle Platt <aplatt@olin.edu>
2021-11-02 15:59:37 -0400

    Basic serial motor control

--- test_motor/test_motor.ino
+++ test_motor/test_motor.ino
@@ -8,19 +8,32 @@ For use with the Adafruit Motor Shield v2
 
 #include <Adafruit_MotorShield.h>
 
+const int numMotors = 1;
+const int bufferLength = 3;
+
+char buff [bufferLength + 1] = {};
+
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
 // Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
+Adafruit_DCMotor *motors [numMotors];
+
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+// Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 // You can also make another motor on port M2
 //Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+
+  for (int i = 0; i <= numMotors; i++) {
+    motors[i] = AFMS.getMotor(i + 1);
+  }
+
 
   if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
   // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
@@ -29,41 +42,24 @@ void setup() {
   }
   Serial.println("Motor Shield found.");
 
-  // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
-  // turn on motor
-  myMotor->run(RELEASE);
+
 }
 
 void loop() {
-  uint8_t i;
+  if (Serial.available() > 2) { 
+    for (int i = 0; i < numMotors; i++){
 
-  Serial.print("tick");
 
-  myMotor->run(FORWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);
-    delay(10);
+      Serial.readBytes(buff, bufferLength);
+      buff[bufferLength] = '\0';
+      int motorSpeed = atoi(buff);
+ 
+      Serial.print(motorSpeed);
+      motors[i]->run(FORWARD);
+      motors[i]->setSpeed(motorSpeed);
+
+    }
+
+    
   }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);
-    delay(10);
-  }
-
-  Serial.print("tock");
-
-  myMotor->run(BACKWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);
-    delay(10);
-  }
-
-  Serial.print("tech");
-  myMotor->run(RELEASE);
-  delay(1000);
 }
