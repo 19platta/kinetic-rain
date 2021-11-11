220955270ec059c532553d6c182cf93697ad7eef
eito-fis <eamon@itofisher.com>
2021-11-10 21:40:07 -0500

    ADD: Refactor to use struct and limit switches

--- sketch_model/test_motor/test_motor.ino
+++ sketch_model/test_motor/test_motor.ino
@@ -8,34 +8,65 @@ For use with the Adafruit Motor Shield v2
 
 #include <Adafruit_MotorShield.h>
 
+typedef struct {
+	uint8_t pin;
+	unsigned long lastDebounceTime;
+	int lastReading;
+	int state;
+} Button;
+
+typedef struct {
+	Adafruit_DCMotor *motor;
+	int lastSpeed;
+	int angle;
+	Button *button;
+} Axle;
+
 const int numMotors = 1;
-Adafruit_DCMotor *motors [numMotors];
+Axle *axles [numMotors];
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+const uint8_t buttonOffset = 0;
+const unsigned long debounceDelay = 50;
 
 const int numCharsPerMotor = 5;
 const byte numChars = numMotors * numCharsPerMotor;
 char receivedChars[numChars];
 boolean newData = false;
 
-void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
 
-
+void setupMotors() {
   for (int i = 0; i < numMotors; i++) {
-    motors[i] = AFMS.getMotor(i + 1);
+		uint8_t pin = i + buttonOffset;
+		pinMode(pin, INPUT);
+    axles[i] = &{
+			.motor = &AFMS.getMotor(i + 1);
+			.lastSpeed = 0;
+			.angle = 0;
+			.button = &{
+				.pin = pin,
+				.lastDebounceTime = 0;
+				.lastReading = LOW;
+				.state = LOW;
+			}
+		}
   }
+}
 
 
-  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+void setup() {
+  Serial.begin(9600);
+
+  if (!AFMS.begin()) {
     Serial.println("Could not find Motor Shield. Check wiring.");
     while (1);
   }
-  Serial.println("Motor Shield found.");
 
+	setupMotors();
 
+  Serial.println("Setup Done");
 }
+
 
 void recvWithStartEndMarkers() {
   /* Reads data from serial, expecting start and end markers
@@ -73,25 +104,50 @@ void recvWithStartEndMarkers() {
   }
 }
 
+
+int readButton(Button *button) {
+	/* Debounces the button.
+   *
+	 * Only updates the button state when the reading has * been consistent for
+	 * at least debounceDelay milliseconds.
+	 */
+	int reading = digitalRead(button->pin);
+	if (reading != button->lastReading) {
+		button->lastDebounceTime = millis();
+	}
+	if ((millis() - button->lastDebounceTime) > debounceDelay) {
+		button->state = reading;
+	}
+	button->lastReading = reading;
+	return button->state;
+}
+
+
 void parseNewData() {
   for (int i = 0; i < numMotors; i++){
     char * token = strtok(i == 0 ? receivedChars : NULL, ",");
     int motorSpeed = atoi(token);
-    Serial.println(motorSpeed);
-    if (motorSpeed > 0) {
-      motors[i]->run(FORWARD);
-    } else {
-      motors[i]->run(BACKWARD);
+		int direction = FORWARD;
+		int limitSwitchReading = readButton(axles[i]->button);
+
+		if (limitSwitchReading == HIGH) {
+			motorSpeed = 0;
+			direction = RELEASE;
+		}
+		else if (motorSpeed < 0) {
       motorSpeed *= -1;
+			direction = BACKWARD;
     }
-    motors[i]->setSpeed(motorSpeed);
+
+		axles[i]->motor->run(direction);
+    axles[i]->motor->setSpeed(motorSpeed);
     newData = false;
   }
 }
 
+
 void loop() {
   recvWithStartEndMarkers();
-  
   if (newData) { 
     parseNewData();
   }
