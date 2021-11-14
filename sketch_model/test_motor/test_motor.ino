94670c82ab729b10a289fd661cc86e19051918d5
eito-fis <eamon@itofisher.com>
2021-11-14 12:56:31 -0500

    FIX: Mutliple motors issue solved

--- sketch_model/test_motor/test_motor.ino
+++ sketch_model/test_motor/test_motor.ino
@@ -25,8 +25,8 @@ const uint8_t shieldPins[numShields] = {0x60};
 // Specify motor pin and button pins with motorPins and buttonPins
 // motorPins[i] length and buttonPins[i] lengt should equal numMotors[i]
 const uint8_t numMotors[numShields] = {1};
-const uint8_t motorPins[numShields][4] = {{1}};
-const uint8_t buttonPins[numShields][4] = {{0}};
+const uint8_t motorPins[numShields][4] = {{1, 3}};
+const uint8_t buttonPins[numShields][4] = {{0, 0}};
 
 // Should be `sum(numMotors)`
 const int numAxles = 1;
@@ -34,12 +34,12 @@ Axle *axles[numAxles];
 
 const unsigned long debounceDelay = 50;
 
-const float maxRotations = 0.5;
+const float maxRotations = 100;
 const float minAngle = 0.0;
 const float maxAngle = 360.0 * maxRotations;
 
-const int numCharsPerMotor = 5;
-const byte numChars = numAxles * numCharsPerMotor;
+const int numCharsPerAxle = 5;
+const byte numChars = numAxles * numCharsPerAxle + 2;
 char receivedChars[numChars];
 boolean newData = false;
 
@@ -51,34 +51,36 @@ void setup() {
 	for (int i = 0; i < numShields; i++) {
 		Adafruit_MotorShield AFMS = Adafruit_MotorShield(shieldPins[i]);
 		if (!AFMS.begin()) {
-			Serial.print("Could not find Motor Shield ");
-			Serial.println(i);
+			Serial.print("Could not find Motor Shield "); Serial.println(i);
 			while (1);
 		}
 
 		for (int j = 0; j < numMotors[i]; j++) {
 			pinMode(buttonPins[i][j], INPUT);
-			Button button = {
+			Button *button = new Button {
 				.pin = buttonPins[i][j],
 				.lastDebounceTime = 0,
 				.lastReading = LOW,
 				.state = LOW,
 			};
-			Axle axle = {
+			Axle *axle = new Axle {
 				.motor = AFMS.getMotor(motorPins[i][j]),
-				.button = &button,
+				.button = button,
 				.lastTime = 0,
 				.angle = 0,
 				.motorSpeed = 0,
 			};
-      delay(0);  // DONT TOUCH
-			axles[axleIdx++] = &axle;
+			axles[axleIdx++] = axle;
+      // DO NOT TOUCH
+      // Running with one motor breaks without this line
+      delay(0); 
 		}
 	}
 
 	if (axleIdx != numAxles) {
-		Serial.print("Only "); Serial.print(axleIdx);
-		Serial.println(" axles setup. Ensure numMotors is set properly.");
+		Serial.print(axleIdx); Serial.println(" axles setup. ");
+    Serial.print(numAxles); Serial.println(" axles expected. ");
+		Serial.println("Ensure numMotors  and numAxles are set properly.");
 		while (1);
 	}
 
@@ -110,9 +112,7 @@ void recvWithStartEndMarkers() {
 				}
 			}
 			else {
-        Serial.print("Test");
 				receivedChars[ndx] = '\0'; // terminate the string
-        Serial.print("Received chars: "); Serial.println(receivedChars);
 				recvInProgress = false;
 				ndx = 0;
 				newData = true;
@@ -140,12 +140,8 @@ void updateAxleAngle(Axle *axle) {
 	// (axle->angle) is potentially a problem, but in practice shouldn't be since
 	// the time between loops is small.
   unsigned long currentTime = millis();
-  Serial.print("Change: "); Serial.println(speed2angle(axle->motorSpeed));
-  Serial.print("Before: "); Serial.println(axle->angle);
 	axle->angle += (float)(currentTime - axle->lastTime) * speed2angle(axle->motorSpeed);
-  Serial.print("After: "); Serial.println(axle->angle);
   axle->lastTime = currentTime;
-  Serial.println();
 }
 
 void updateAngles() {
@@ -192,7 +188,7 @@ void setMotor(Axle *axle, int motorSpeed, int dir) {
 
 void parseNewData() {
 	for (int i = 0; i < numAxles; i++) {
-		char * token = strtok(i == 0 ? receivedChars : NULL, ",");
+		char * token = strtok(i == 0 ? receivedChars : NULL, ",>");
 		int motorSpeed = atoi(token);
 		int dir = FORWARD;
     
@@ -213,8 +209,8 @@ void parseNewData() {
 		}
 
 		setMotor(axles[i], motorSpeed, dir);
-		newData = false;
 	}
+  newData = false;
 }
 
 
@@ -227,5 +223,4 @@ void loop() {
 	else {
     updateAngles();
 	}
-  Serial.println(axles[0]->angle);
 }
